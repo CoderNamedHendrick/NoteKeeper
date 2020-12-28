23024cfbe89b29f0e8730c8c1434b74ea7ec8e80
Hendrick <sebastinesoacatp@gmail.com>
2020-12-28 12:58:26 +0100

    Building the note list activity

---
+++ app/src/main/java/com/example/coursenotekeeper/NoteActivity.java
@@ -0,0 +1,46 @@
+package com.example.coursenotekeeper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
+
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/coursenotekeeper/NoteInfo.java
@@ -0,0 +1,65 @@
+package com.example.coursenotekeeper;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public final class NoteInfo {
+    private CourseInfo mCourse;
+    private String mTitle;
+    private String mText;
+
+    public NoteInfo(CourseInfo course, String title, String text) {
+        mCourse = course;
+        mTitle = title;
+        mText = text;
+    }
+
+    public CourseInfo getCourse() {
+        return mCourse;
+    }
+
+    public void setCourse(CourseInfo course) {
+        mCourse = course;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    private String getCompareKey() {
+        return mCourse.getCourseId() + "|" + mTitle + "|" + mText;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NoteInfo that = (NoteInfo) o;
+
+        return getCompareKey().equals(that.getCompareKey());
+    }
+
+    @Override
+    public int hashCode() {
+        return getCompareKey().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getCompareKey();
+    }
+
+}
