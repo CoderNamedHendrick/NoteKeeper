fb8ab95acf24bab1bb3622fabd62ae800d4a4a5b
Hendrick <sebastinesoacatp@gmail.com>
2020-12-30 19:23:11 +0100

    Creating and automating androidTests.

---
+++ app/src/androidTest/java/com/example/coursenotekeeper/NoteCreationTest.java
@@ -0,0 +1,77 @@
+package com.example.coursenotekeeper;
+
+import android.os.BadParcelableException;
+import android.util.Log;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static org.junit.Assert.*;
+
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void classSetUp(){
+        sDataManager = DataManager.getInstance();
+    }
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivityActivityTestRule =
+            new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNewNote(){
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of our test note";
+
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        Log.d(getClass().getSimpleName(), "We are in Note Activity");
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle())
+        )));
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                .check(matches(withText(containsString(noteTitle))));
+
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
+                closeSoftKeyboard());
+
+        onView(withId(R.id.text_note_text))
+                .check(matches(withText(containsString(noteText))));
+        Log.d(getClass().getSimpleName(), "Done inputting data");
+
+        try {
+            pressBack();
+        } catch (BadParcelableException e){
+            e.printStackTrace();
+        }
+
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+    }
+
+}
