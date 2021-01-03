f886b18916fc219c0c8b318fab9a14bb4e160d2f
Hendrick <sebastinesoacatp@gmail.com>
2021-01-03 14:06:59 +0100

    Tests

---
+++ app/src/androidTest/java/com/example/coursenotekeeper/NextThroughNoteTest.java
@@ -0,0 +1,55 @@
+package com.example.coursenotekeeper;
+
+import static org.junit.Assert.*;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static org.junit.Assert.*;
+
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+
+public class NextThroughNoteTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void NextThroughNote(){
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_note));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+        NoteInfo note = notes.get(index);
+
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle()))
+        );
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+    }
+}
