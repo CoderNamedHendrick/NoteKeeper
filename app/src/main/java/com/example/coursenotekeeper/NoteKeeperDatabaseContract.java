63ebf351ccfdc295527405a80a5c06db7ff6a1be
Hendrick <sebastinesoacatp@gmail.com>
2021-01-12 01:19:16 +0100

    Creating the contract class for our sqlite content provider

--- app/src/main/java/com/example/coursenotekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/coursenotekeeper/NoteKeeperDatabaseContract.java
@@ -14,4 +14,18 @@ public final class NoteKeeperDatabaseContract {
                         COLUMN_COURSE_ID + ", " +
                         COLUMN_COURSE_TITLE + ")";
     }
+
+    public static final class NoteInfoEntry{
+        public static final String TABLE_NAME = "note_info";
+        public static final String COLUMN_COURSE_ID = "course_id";
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+
+        // CREATE TABLE note_info (note_title, note_text, course_id)
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_NOTE_TITLE + ", " +
+                        COLUMN_NOTE_TEXT + ", " +
+                        COLUMN_COURSE_ID + ")";
+    }
 }
