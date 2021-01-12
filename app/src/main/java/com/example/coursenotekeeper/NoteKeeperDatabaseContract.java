017a280879956bc6baee0d737b8bb563e3f306cb
Hendrick <sebastinesoacatp@gmail.com>
2021-01-12 01:28:54 +0100

    Creating the contract class for our sqlite content provider

--- app/src/main/java/com/example/coursenotekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/coursenotekeeper/NoteKeeperDatabaseContract.java
@@ -1,31 +1,35 @@
 package com.example.coursenotekeeper;
 
+import android.provider.BaseColumns;
+
 public final class NoteKeeperDatabaseContract {
     private NoteKeeperDatabaseContract(){} // make non-creatable
 
-    public static final class CourseInfoEntry{
+    public static final class CourseInfoEntry implements BaseColumns {
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
-        // CREATE TABLE course_info (course_id, course_title)
+        // CREATE TABLE course_info (course_id TEXT UNIQUE NOT NULL, course_title TEXT NOT NULL)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_COURSE_ID + ", " +
-                        COLUMN_COURSE_TITLE + ")";
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, " +
+                        COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
-    public static final class NoteInfoEntry{
+    public static final class NoteInfoEntry implements BaseColumns{
         public static final String TABLE_NAME = "note_info";
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
 
-        // CREATE TABLE note_info (note_title, note_text, course_id)
+        // CREATE TABLE note_info (note_title TEXT NOT NULL, note_text, course_id TEXT NOT NULL)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + " (" +
-                        COLUMN_NOTE_TITLE + ", " +
+                        _ID + " INTEGER PRIMARY KEY, " +
+                        COLUMN_NOTE_TITLE + " TEXT NOT NULL, " +
                         COLUMN_NOTE_TEXT + ", " +
-                        COLUMN_COURSE_ID + ")";
+                        COLUMN_COURSE_ID + " TEXT NOT NULL)";
     }
 }
