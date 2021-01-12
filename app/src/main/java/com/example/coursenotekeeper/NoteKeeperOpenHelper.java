7215e695510c60e1fcb601a643f8cef769a948b5
Hendrick <sebastinesoacatp@gmail.com>
2021-01-12 01:39:23 +0100

    Creating the sqlite database

---
+++ app/src/main/java/com/example/coursenotekeeper/NoteKeeperOpenHelper.java
@@ -0,0 +1,27 @@
+package com.example.coursenotekeeper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "NoteKeeper.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public NoteKeeperOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
